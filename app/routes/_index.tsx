380d8f0d3b3c9fb81ede57e9ac669da74b1a9d7c
xulihang <xulihanghai@163.com>
2023-11-14 17:11:45 +0800

    use the scanner page to add new books

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { json } from "@remix-run/node";
 import { getBooks } from "../data";
 import styles from "~/styles/camera.css";
 import { BarcodeReader, TextResult } from "dynamsoft-javascript-barcode";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData,useNavigate } from "@remix-run/react";
 import BookCard from "~/components/BookCard";
 import FloatingActionButton from "~/components/FloatActionButton";
 
@@ -28,29 +28,11 @@ export const loader = async () => {
 
 
 export default function Index() {
+  const navigate = useNavigate();
   const { books } = useLoaderData<typeof loader>();
-  const [isActive,setIsActive] = useState(false);
-  const [initialized,setInitialized] = useState(false);
-  const [barcodes,setBarcodes] = useState<TextResult[]>([]);
-  const startBarcodeScanner = () => {
-    if (initialized) {
-      setIsActive(true);
-    }else{
-      alert("Please wait for the initialization and then try again.");
-    }
-  }
-  const stopBarcodeScanner = () => {
-    setIsActive(false);
-  }
   return (
     <div>
-      <h1>Remix Barcode Scanner</h1>
-      <button onClick={()=>startBarcodeScanner()} >Start Scanning</button>
-      <ol>
-        {barcodes.map((barcode,idx)=>(
-          <li key={idx}>{barcode.barcodeFormatString+": "+barcode.barcodeText}</li>
-        ))}
-      </ol>
+      <h1>Remix Book Manager</h1>
       {books.map((bookRecord,idx)=>(
         <BookCard record={bookRecord} key={"book-card-"+idx}>
           <Link to={`/books/`+bookRecord.ISBN}>View</Link>
@@ -59,25 +41,8 @@ export default function Index() {
       <div className="fab-container">
         <FloatingActionButton onClicked={()=>{
           console.log("clicked");
+          navigate("/scanner");
         }}></FloatingActionButton>
-      </div>
-      <div className="scanner" style={{display:isActive?"":"none"}}>
-        <BarcodeScanner 
-          onInitialized={async (_enhancer:CameraEnhancer,reader:BarcodeReader)=>{
-            const settings = await reader.getRuntimeSettings();
-            settings.expectedBarcodesCount = 0;
-            await reader.updateRuntimeSettings(settings);
-            setInitialized(true);
-          }} 
-          onScanned={(results)=> {
-            console.log(results);
-            setIsActive(false);
-            setBarcodes(results);
-          }}
-          isActive={isActive}
-        >
-          <button style={{position:"absolute",right:0}} onClick={()=>stopBarcodeScanner()} >Close</button>
-        </BarcodeScanner>
       </div>
     </div>
   );
---
+++ app/routes/scanner.tsx
@@ -0,0 +1,108 @@
+import { type MetaFunction, type LinksFunction, redirect, ActionFunctionArgs } from "@remix-run/node";
+import { useState } from "react";
+import BarcodeScanner from "~/components/BarcodeScanner";
+import {CameraEnhancer} from "dynamsoft-camera-enhancer";
+import styles from "~/styles/camera.css";
+import { BarcodeReader, TextResult } from "dynamsoft-javascript-barcode";
+import { Form } from "@remix-run/react";
+import { addBook } from "~/data";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
+
+
+  export const action = async ({
+    params,
+    request,
+  }: ActionFunctionArgs) => {
+
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+    const title = updates.title.toString();
+    const author = updates.author.toString();
+    const ISBN = updates.ISBN.toString();
+    const timeStamp = new Date().getTime().toString();
+    addBook({
+      title:title,
+      author:author,
+      ISBN:ISBN,
+      createdAt:timeStamp
+    })
+    return redirect(`/`);
+  };
+
+
+export default function Scanner() {
+  const [isActive,setIsActive] = useState(false);
+  const [initialized,setInitialized] = useState(false);
+  const [ISBN,setISBN] = useState("");
+  const [author,setAuthor] = useState("");
+  const [title,setTitle] = useState("");
+  const startBarcodeScanner = () => {
+    if (initialized) {
+      setIsActive(true);
+    }else{
+      alert("Please wait for the initialization and then try again.");
+    }
+  }
+  const stopBarcodeScanner = () => {
+    setIsActive(false);
+  }
+  return (
+    <div>
+      <h1>New Book</h1>
+      
+      <Form id="book-form" method="post">
+        <div>
+          <label>
+            ISBN:
+            <input name="ISBN" onChange={e=>setISBN(e.target.value)} type="text" value={ISBN}/>
+          </label>
+          <button type="button" onClick={()=>startBarcodeScanner()} >Scan</button>
+        </div>
+        <div>
+          <label>
+            Title:
+            <input name="title" onChange={e=>setTitle(e.target.value)} type="text" value={title}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Author:
+            <input name="author" onChange={e=>setAuthor(e.target.value)} type="text" value={author}/>
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
+      <div className="scanner" style={{display:isActive?"":"none"}}>
+        <BarcodeScanner 
+          onInitialized={async (_enhancer:CameraEnhancer,reader:BarcodeReader)=>{
+            const settings = await reader.getRuntimeSettings();
+            settings.expectedBarcodesCount = 0;
+            await reader.updateRuntimeSettings(settings);
+            setInitialized(true);
+          }} 
+          onScanned={(results)=> {
+            console.log(results);
+            setIsActive(false);
+            if (results.length>0) {
+              console.log("set ISBN");
+              setISBN(results[0].barcodeText);
+            }
+          }}
+          isActive={isActive}
+        >
+          <button style={{position:"absolute",right:0}} onClick={()=>stopBarcodeScanner()} >Close</button>
+        </BarcodeScanner>
+      </div>
+    </div>
+  );
+}
